50d7289bf3b644f0b543e275b3a33efbe815399e
kamiokz123 <kamranhaiderokz888@gmail.com>
2024-10-10 13:44:09 -0700

    some new changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,23 +6,23 @@ function Navbar() {
     const navItems = [
         {
             id: 1,
-            link: <a href="#home" onClick={() => setShowMenu(!showMenu)} className='hover:text-white hover:scale-105 cursor-pointer'>Home</a>
+            link: <a href="#home"  className='hover:text-white hover:scale-105 cursor-pointer'>Home</a>
         },
         {
             id: 2,
-            link: <a href="#about" onClick={() => setShowMenu(!showMenu)} className='hover:text-white hover:scale-105 cursor-pointer'>About</a>
+            link: <a href="#about" className='hover:text-white hover:scale-105 cursor-pointer'>About</a>
         },
         {
             id: 3,
-            link: <a href="#portfolio" onClick={() => setShowMenu(!showMenu)} className='hover:text-white hover:scale-105 cursor-pointer'>Portfolio</a>
+            link: <a href="#portfolio" className='hover:text-white hover:scale-105 cursor-pointer'>Portfolio</a>
         },
         {
             id: 4,
-            link: <a href="#skills" onClick={() => setShowMenu(!showMenu)} className='hover:text-white hover:scale-105 cursor-pointer'>Skills</a>
+            link: <a href="#skills" className='hover:text-white hover:scale-105 cursor-pointer'>Skills</a>
         },
         {
             id: 5,
-            link: <a href="#contact" onClick={() => setShowMenu(!showMenu)} className='hover:text-white hover:scale-105 cursor-pointer'>Contact</a>
+            link: <a href="#contact" className='hover:text-white hover:scale-105 cursor-pointer'>Contact</a>
         }
     ]
     return (
@@ -37,7 +37,7 @@ function Navbar() {
                             className='px-3 text-gray-400 font-bold   duration-200'
                         >{link}</li>
                     ))}
-                        <li className='px-3 text-gray-400 font-bold   duration-200 '><a className='hover:text-white hover:scale-105 cursor-pointer' href="/my-resume.pdf" download={"kamran-haider-resume.pdf"}>Resume</a></li>
+                        <li  className='px-3 text-gray-400 font-bold  duration-200 '><a className='hover:text-white hover:scale-105 cursor-pointer' href="/my-resume.pdf" download={"kamran-haider-resume.pdf"}>Resume</a></li>
 
                 </ul>
                 <div onClick={() => setShowMenu(!showMenu)} className='px-3 md:hidden'>
@@ -49,10 +49,11 @@ function Navbar() {
                             <li
                                 key={id}
                                 className='px-3 mt-5   font-bold text-gray-400 hover:scale-105 duration-200 cursor-pointer'
+                                onClick={() => setShowMenu(!showMenu)}
                             >{link}</li>
                             
                         ))}
-                        <li className='px-3 mt-5  text-gray-400 font-bold   duration-200 '><a className='hover:text-white hover:scale-105 cursor-pointer' href="/my-resume.pdf" download={"kamran-haider-resume.pdf"}>Resume</a></li>
+                        <li onClick={() => setShowMenu(!showMenu)} className='px-3 mt-5  text-gray-400 font-bold   duration-200 '><a className='hover:text-white hover:scale-105 cursor-pointer' href="/my-resume.pdf" download={"kamran-haider-resume.pdf"}>Resume</a></li>
                     </ul>
                 )}
             </nav>
